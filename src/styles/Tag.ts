3a87637308012c17fc064db4154e0e82810a17e4
David Felipe <felipe30.10@outlook.com>
2025-03-08 16:09:34 -0300

    create component style Tag

---
+++ src/styles/Tag.ts
@@ -0,0 +1,9 @@
+import { styled } from "../../stitches.config";
+
+export const Tag = styled('strong', {
+    fontFamily: '$jetbrains_mono',
+    fontSize: '$md',
+    fontWeight: '$semibold',
+    lineHeight: '$short',
+    letterSpacing: '4%'
+})
