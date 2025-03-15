0f0b28f24efc029ff034c61e61985498a4fa5888
David Felipe <felipe30.10@outlook.com>
2025-03-15 15:51:58 -0300

    create pages projects and blog

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,8 @@
+export function Blog() {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>Blog page content</p>
+        </div>
+    )
+}
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,8 @@
+export function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Projects page content</p>
+    </div>
+  );
+}
